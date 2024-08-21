ae5f7a5b322184e604e2ce980508bfa98a31708e
Wes Koerber <wes@weskoerber.com>
2024-08-21 01:34:13 -0400

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# "Hello, World!" Comparison
+
+![Plotted Results](img/chart.svg "Plotted results")
---
+++ test.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+mkdir -p build results
+
+hyperfine --export-json results/gcc.json -N --warmup 10 'gcc -g -o build/main-c-gcc c/main.c'
+hyperfine --export-json results/clang.json -N --warmup 10 'clang -g -o build/main-c-clang c/main.c'
+hyperfine --export-json results/g++.json -N --warmup 10 'g++ -g -o build/main-cpp-g++ cpp/main.cpp'
+hyperfine --export-json results/clang++.json -N --warmup 10 'clang++ -g -o build/main-cpp-clang++ cpp/main.cpp'
+hyperfine --export-json results/rust-llvm.json -N --warmup 10 'rustc -o build/main-rust-llvm rust/main.rs'
+hyperfine --export-json results/rust-cranelift.json -N --warmup 10 'rustc -Zcodegen-backend=cranelift -o build/main-rust-cranelift rust/main.rs'
+
+cd build
+hyperfine --export-json ../results/zig-llvm.json -N --warmup 10 'zig build-exe --name main-zig-llvm ../zig/main.zig'
+hyperfine --export-json ../results/zig-zig.json -N --warmup 10 'zig build-exe -fno-llvm --name main-zig-zig ../zig/main.zig'
+cd ..
+
+results=""
+count=0
+
+csv_header() {
+    printf ","
+    i=1
+    for f in $results; do
+        run="${f%.json}"
+
+        [ $i -lt $count ] && printf "%s," $run || printf "%s\n" $run
+
+        i=$(( i + 1 ))
+    done
+}
+
+csv_metric() {
+    printf "%s," "$1"
+    i=1
+    for f in $results; do
+        value=$(jq -r ".results[0].$1" results/$f)
+
+        [ $i -lt $count ] && printf "%f," $value || printf "%f\n" $value
+
+        i=$(( i + 1 ))
+    done
+}
+
+results=$(ls results)
+count=$(ls -1 results | wc -l)
+
+csv_header
+csv_metric "mean"
+csv_metric "median"
+csv_metric "stddev"
